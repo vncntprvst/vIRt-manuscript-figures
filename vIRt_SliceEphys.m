eb1e2f58ed4ad77c73d492e4233e29308d2b21a0
Vincent <prevosto@mit.edu>
2022-04-09 17:40:38 -0400

    updates SliceEphys

--- vIRt_SliceEphys.m
+++ vIRt_SliceEphys.m
@@ -8,75 +8,80 @@ sr=struct('date',[],'filename',[],'event',[],'trace',[],...
     'interval',[],'header',[]);
 rNum=0;
 
-for fileNum = 1:length(files)
-    fName=fullfile(files(fileNum).folder,files(fileNum).name);
-    [~,recDate]=fileparts(files(fileNum).folder);
-    filename=files(fileNum).name;
-    try
-        [data,interval,header] = abfload(fName) ;
-        trace=reshape(squeeze(data(:,1,:)),[numel(data(:,1,:)), 1])';
-        clearvars data
-    catch
-        continue
+if exist(fullfile(cd,'sliceRecs.mat'),'file')
+    load('sliceRecs.mat');
+else
+    for fileNum = 1:length(files)
+        fName=fullfile(files(fileNum).folder,files(fileNum).name);
+        [~,recDate]=fileparts(files(fileNum).folder);
+        filename=files(fileNum).name;
+        try
+            [data,interval,header] = abfload(fName) ;
+            trace=reshape(squeeze(data(:,1,:)),[numel(data(:,1,:)), 1])';
+            clearvars data
+        catch
+            continue
+        end
+        % end
+        
+        if false
+            figure; hold on
+            plot(trace(1:1000))
+            %     plot(Primary__.values(1:1000)) %Spike export - for comparison
+        end
+        
+        %% create timeline
+        tStep=interval/10^6;% sampling interval in us
+        timestamps = 0:tStep:numel(trace)*tStep;
+        %% get notes
+        notes=header.tags;
+        
+        for evtNum=1:size(notes,2)
+            
+            eventTime=notes(evtNum).timeSinceRecStart;
+            preTime= 5*60; %
+            postTime= 5*60;
+            evWindow=int32((eventTime-preTime)/tStep:(eventTime+postTime)/tStep);
+            evWindow=evWindow(evWindow>=1);evWindow=evWindow(evWindow<=numel(trace));
+            filtTrace=PreProcData(trace(evWindow),1/tStep,{'bandpass',[5 6000]});
+            wTimestamps=timestamps(evWindow);
+            threshold = -rms(filtTrace)*5;
+            
+            %% get spike times
+            spikes = logical(filtTrace < threshold);
+            spikeProp = regionprops(spikes, 'Area', 'PixelIdxList');
+            spikeIdx = cellfun(@(timeIndex) timeIndex(1), {spikeProp.PixelIdxList});
+            spikeTimes = wTimestamps(spikeIdx);
+            
+            %% compute ISI CV2
+            ISI_pre=diff(spikeTimes(spikeTimes<eventTime));
+            CV2_pre=2*[abs(diff(ISI_pre)) NaN]./movsum(ISI_pre,2);
+            ISI_post=diff(spikeTimes(spikeTimes>=eventTime));
+            CV2_post=2*[abs(diff(ISI_post)) NaN]./movsum(ISI_post,2);
+            
+            %% store data
+            rNum=rNum+1;
+            sr(rNum).filename=filename;
+            sr(rNum).date=recDate;
+            sr(rNum).interval=interval;
+            sr(rNum).header=header;
+            sr(rNum).event=deblank(notes(evtNum).comment);
+            sr(rNum).eventTime=eventTime;
+            sr(rNum).trace=filtTrace;
+            sr(rNum).spikeTimes=spikeTimes;
+            sr(rNum).ISI={ISI_pre;ISI_post};
+            sr(rNum).CV2={CV2_pre;CV2_post};
+        end
+        clearvars trace interval header notes
     end
-    % end
-    
-    if false
-        figure; hold on
-        plot(trace(1:1000))
-        %     plot(Primary__.values(1:1000)) %Spike export - for comparison
-    end
-    
-    %% create timeline
-    tStep=interval/10^6;% sampling interval in us
-    timestamps = 0:tStep:numel(trace)*tStep;
-    %% get notes
-    notes=header.tags;
-    
-    for evtNum=1:size(notes,2)
-        
-        eventTime=notes(evtNum).timeSinceRecStart;
-        preTime= 5*60; %
-        postTime= 5*60;
-        evWindow=int32((eventTime-preTime)/tStep:(eventTime+postTime)/tStep);
-        evWindow=evWindow(evWindow>=1);evWindow=evWindow(evWindow<=numel(trace));
-        filtTrace=PreProcData(trace(evWindow),1/tStep,{'bandpass',[5 6000]});
-        wTimestamps=timestamps(evWindow);
-        threshold = -rms(filtTrace)*5;
-        
-        %% get spike times
-        spikes = logical(filtTrace < threshold);
-        spikeProp = regionprops(spikes, 'Area', 'PixelIdxList');
-        spikeIdx = cellfun(@(timeIndex) timeIndex(1), {spikeProp.PixelIdxList});
-        spikeTimes = wTimestamps(spikeIdx);
-        
-        %% compute ISI CV2
-        ISI_pre=diff(spikeTimes(spikeTimes<eventTime));
-        CV2_pre=2*[abs(diff(ISI_pre)) NaN]./movsum(ISI_pre,2);
-        ISI_post=diff(spikeTimes(spikeTimes>=eventTime));
-        CV2_post=2*[abs(diff(ISI_post)) NaN]./movsum(ISI_post,2);
-        
-        %% store data
-        rNum=rNum+1;
-        sr(rNum).filename=filename;
-        sr(rNum).date=recDate;
-        sr(rNum).interval=interval;
-        sr(rNum).header=header;
-        sr(rNum).event=deblank(notes(evtNum).comment);
-        sr(rNum).eventTime=eventTime;
-        sr(rNum).trace=filtTrace;
-        sr(rNum).spikeTimes=spikeTimes;
-        sr(rNum).ISI={ISI_pre;ISI_post};
-        sr(rNum).CV2={CV2_pre;CV2_post};
-    end
-    clearvars trace interval header notes
+    save('sliceRecs','sr','-v7.3');
 end
-save('sliceRecs','sr','-v7.3');
 
 %%
 doPlots = false;
 if doPlots
     recList=find(contains({sr.event},'4AP'));
+    recList=[33,34,35,38,41,42,39,40,44,49,48,64,62,63]; %1:64; 
     sr_p=sr(recList);
     
     % check 10/26/21, 11/03/21, 1/18/2022, 02/17/22
@@ -115,11 +120,11 @@ if doPlots
     axis off
     subplot(10,2,3:2:19)
     histogram(ISI_pre,0:0.005:0.2)
-    xlabel('ISI (ms)')
+    xlabel('ISI (s)')
     title(['pre ' deblank(sr_p(rNum).event)])
     subplot(10,2,4:2:20)
     histogram(ISI_post,0:0.005:0.2)
-    xlabel('ISI (ms)')
+    xlabel('ISI (s)')
     title(['post ' deblank(sr_p(rNum).event)])
     
 %     figure('color','white','position',[871   460   560   420])
@@ -133,21 +138,32 @@ if doPlots
 %     title(['post ' deblank(notes(evtNum).comment)])
     
     end
-    
+    rNum=7;
     %% plot trace around event times
+    tStep=sr_p(rNum).interval/10^6;% sampling interval in us
+    timestamps = 0:tStep:numel(sr_p(rNum).trace)*tStep;
+    eventTime=sr_p(rNum).eventTime;
+    preTime= 5*60; %
+    %     postTime= 5*60;
+    %     evWindow=int32((eventTime-preTime)/tStep:(eventTime+postTime)/tStep);
+    %     evWindow=evWindow(evWindow>=1);evWindow=evWindow(evWindow<=numel(sr_p(rNum).trace));
+    wTimestamps=timestamps(1:end-1);
+    ISI_pre=sr_p(rNum).ISI{1};
+    ISI_post=sr_p(rNum).ISI{2};
+    
     figure('color','white','position',[21 478 1866 444])
-    plot(wTimestamps,filtTrace,'k')
-    xline(eventTime,'--r',notes(evtNum).comment,'LineWidth',1);
-    yline(threshold,'--g','spike threshold')
+    plot(wTimestamps,sr_p(rNum).trace,'k')
+    xline(preTime,'--r',sr_p(rNum).event,'LineWidth',1);
+    %     yline(threshold,'--g','spike threshold')
     xlabel('time (s)')
     ylabel(['current amplitude (' (sr_p(rNum).header.recChUnits{1}) ')'])
     title({['recording day ' sr_p(rNum).date...
         ' file ' sr_p(rNum).filename(1:end-4)];...
-        ['Condition: ' deblank(notes(evtNum).comment) ...
-        ' CV2_pre: ' num2str(nanmean(CV2_pre))...
-        ' CV2_post: ' num2str(nanmean(CV2_post))]},'interpreter','none')
-    hold on
-    plot(spikeTimes,ones(numel(spikeTimes),1)*(min(filtTrace)-1),'bd')
+        ['Condition: ' deblank(sr_p(rNum).event) ...
+        ' ISI_pre: ' num2str(nanmean(ISI_pre))...
+        ' ISI_post: ' num2str(nanmean(ISI_post))]},'interpreter','none')
+%     hold on
+%     plot(sr_p(rNum).spikeTimes,ones(numel(spikeTimes),1)*(min(sr_p(rNum).trace)-1),'bd')
     
     %% save figure
     if ~exist(fullfile(cd, 'Figures'),'dir')
@@ -160,10 +176,7 @@ if doPlots
         sr_p(rNum).filename(1:end-4)...
         '_' deblank(notes(evtNum).comment)]),'-dpng');
     close(gcf)
-    
-    
 
-    
     %% Plot Rhythmicity
     % Find first spike of each burst, plot distribution
     postST=spikeTimes(spikeTimes>=eventTime);
